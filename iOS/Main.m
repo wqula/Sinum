0263915a9898fb406f8f7ff99ac7aa7da908a82b
Wqula <329488268+wqula@users.noreply.github.com>
2026-09-18 18:49:49 +0900

    i made a stupid mistake

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://127.0.0.1"
+#define API_URL @"http://127.0.0.1:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
